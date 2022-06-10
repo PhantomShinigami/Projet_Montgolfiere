a67e9064c688d6cc111e503b9adbe762c30ede4e
Gwendo8 <77733860+Gwendo8@users.noreply.github.com>
2022-06-10 18:48:59 +0200

    Add files via upload

--- capteur_de_pression/capteur_de_pression.ino
+++ capteur_de_pression/capteur_de_pression.ino
@@ -1,30 +1,118 @@
-//#include <LiquidCrystal_I2C.h> //inclure la bibliothèque
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-//LiquidCrystal_I2C lcd(0x3F, 20, 4); //définir les caractéristiques de l'écran LCD
+const char* ssid = "CCF_Montgolfiere"; //wifi
+const char* password = "";
+
+const char* mqtt_server = "192.168.107.136";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long now = millis();
+long lastMeasure = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", "usersnir2", "projetmontgolfiere")) {
+      Serial.println("connected");
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+
+    }
+    
+    else {
+      Serial.print("");
+      Serial.print(client.state());
+      Serial.println("");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+    
+  }
+}
+
 
 void setup()
 {
   Serial.begin(112500);
-  //lcd.init(); //initialiser l'écran LCD
-  //lcd.backlight(); //allumer le rétroéclairage
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
 }
+
+
 void loop()
 {
-  int sensorVal=analogRead(A0); //lire la valeur sur le pin A0
 
-  float voltage= (sensorVal*5.0)/1024.0; //convertir le signal analogique 0-5Volt en signal 0-1024
+  int sensorVal=analogRead(A0); //lire la valeur sur le pin A0
+  
+
+  float voltage= (sensorVal*3.3)/1024.0; //convertir le signal analogique 0-5Volt en signal 0-1024
 
   float pressure_pascal = (3.0*((float)voltage-0.47))*1000000.0; //convertir le signal en une pression pascal
-  float pressure_bar = pressure_pascal/10e5; //convertir la pression de pascals à bars
+  float pressure_bar = pressure_pascal/10e5 *0.83; //convertir la pression de pascals à bars
+  
+  
+  char result[8];
+  dtostrf(pressure_bar,6,2,result);
+ 
+    if (!client.connected()) {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("ESP8266Client");
 
+  now = millis();
+  // Publishes new temperature and humidity every 30 seconds
+  if (now - lastMeasure > 30000) {
+    lastMeasure = now;
+
+  
+  
+  client.publish("room/pression", result);
+  
+  
   //lcd.setCursor(0,0);
   Serial.print("Pression="); //ecire le message entre les guillemets
 
   //lcd.setCursor(5,0);
-  Serial.print(pressure_bar*0.83); //écrire la variable entre les parenthèses
+  Serial.print(pressure_bar); //écrire la variable entre les parenthèses
   //lcd.print(" bars"); //écrire le message entre les guillemets
-  delay(100);
+  delay(1000);
+  }
 }
